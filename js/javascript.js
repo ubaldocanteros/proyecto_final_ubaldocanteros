d1e4a39205de98d038787f6943bfc0f044234ce8
uby <ubaldodavidcanteros@gmail.com>
2022-12-13 08:52:04 -0300

    add: styles

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,29 @@
 /* Pagina Principal */
 
 
+var edad = parseInt(prompt("Bienbenido. Por favor introduce tu edad"));
 
+
+    if(edad>=18){
+        alert("Puedes entrar, eres mayor de edad.");
+    }else{
+        alert("Eres menor de edad, entras bajo tu responsabilidad");
+    }  
+
+
+
+
+    let goleadoreslink = document.getElementById("goleadoreslink")
+    goleadoreslink.addEventListener("click", clicklink)
+
+function clicklink(){
+    if(confirm("Vas a dirigirte a una pagina externa.Deseas continuar")){
+    location.href = "https://es.wikipedia.org/wiki/Goleador_(f%C3%BAtbol)#M%C3%A1ximos_goleadores_de_la_historia_del_f%C3%BAtbol";
+    
+}else{
+    alert("Aca nos quedamos.")
+}  
+}
 
 
 /* Lista Jugadores */
